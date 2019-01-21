3f45477f50ceee3c43544a1f37d1e906039acd5a
LFalch <lucas@wasd.dk>
2019-01-21 19:01:25 +0100

    Re-factor and idiomise

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-#[macro_use]
-extern crate text_io;
+#![warn(clippy::all)]
+
+use std::{
+    fmt::{self, Display},
+    io::{Write, stdout},
+    ops::Not,
+};
+use text_io::{read, try_read, try_scan};
 use ansi_term::Colour;
-use std::io::stdout;
-use std::io::Write;
 
 fn clear() {
     // TODO: Make this work on Windows #1
@@ -10,101 +14,141 @@ fn clear() {
     print!("{}[2J", 27 as char);
 }
 
-fn colourize(piece: &str) -> String {
-    match piece {
-        "X" => Colour::Blue.bold().paint(piece).to_string(),
-        "O" => Colour::Yellow.bold().paint(piece).to_string(),
-        _ => piece.to_string(),
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+enum Player {
+    X,
+    O,
+}
+
+impl Player {
+    #[inline]
+    fn as_str(self) -> &'static str {
+        match self {
+            Player::X => "X",
+            Player::O => "O",
+        }
     }
-}
+    fn colourize(self) -> String {
+        match self {
+            Player::X => Colour::Blue,
+            Player::O => Colour::Yellow,
+        }
+        .bold()
+        .paint(self.as_str())
+        .to_string()
+    }
+    fn get_move(self) -> usize {
+        loop {
+            print!("Player {}, place your marker: ", self.colourize());
+            stdout().flush().expect("Could not flush stdout.");
+            let input: String = read!("{}\n");
 
-fn draw_board(board: [&str; 9]) {
-    let tab = " ".repeat(9);
-    println!("");
-    println!(
-        "{} {} | {} | {}",
-        tab,
-        colourize(&board[0]),
-        colourize(&board[1]),
-        colourize(&board[2])
-    );
-    println!("{}--- --- ---", tab);
-    println!(
-        "{} {} | {} | {}",
-        tab,
-        colourize(&board[3]),
-        colourize(&board[4]),
-        colourize(&board[5])
-    );
-    println!("{}--- --- ---", tab);
-    println!(
-        "{} {} | {} | {}",
-        tab,
-        colourize(&board[6]),
-        colourize(&board[7]),
-        colourize(&board[8])
-    );
-    println!("");
-}
-
-fn get_move(player: &str) -> usize {
-    loop {
-        print!("Player {}, place your marker: ", colourize(&player));
-        stdout().flush().expect("Could not flush stdout.");
-        let input: String = read!("{}\n");
-
-        match input.trim().parse::<usize>() {
-            Ok(n @ 1...9) => return n - 1,
-            Ok(_) => println!("Outside the board.. 🤦‍"),
-            Err(_) => println!("Invalid input."),
+            match input.trim().parse::<usize>() {
+                Ok(n @ 1..=9) => return n - 1,
+                Ok(_) => println!("Outside the board.. 🤦‍"),
+                Err(_) => println!("Invalid input."),
+            }
         }
     }
 }
 
-fn winner(board: [&str; 9]) -> bool {
-    (board[0] == board[1]) && (board[1] == board[2])
-        || (board[3] == board[4]) && (board[4] == board[5])
-        || (board[6] == board[7]) && (board[7] == board[8])
-        || (board[0] == board[3]) && (board[3] == board[6])
-        || (board[0] == board[4]) && (board[4] == board[8])
-        || (board[1] == board[4]) && (board[4] == board[7])
-        || (board[2] == board[5]) && (board[5] == board[8])
-        || (board[2] == board[4]) && (board[4] == board[6])
+impl Display for Player {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        self.as_str().fmt(f)
+    }
 }
 
-fn available(board: [&str; 9], placement: usize) -> bool {
-    board[placement] != "X" && board[placement] != "O"
+impl Not for Player {
+    type Output = Self;
+    #[inline]
+    fn not(self) -> Self::Output {
+        match self {
+            Player::X => Player::O,
+            Player::O => Player::X,
+        }
+    }
+}
+
+#[derive(Default, Debug, Copy, Clone, PartialEq, Eq)]
+struct Board {
+    board: [Option<Player>; 9],
+}
+
+macro_rules! tab {
+    () => {
+        "         "
+    };
+}
+/// 9 spaces
+const TAB: &str = tab!();
+const WIN_CASES: &[(usize, usize, usize)] = &[(0, 1, 2), (3, 4, 5), (6, 7, 8), (0, 3, 6), (0, 4, 8), (1, 4, 7), (2, 5, 8), (2, 4, 6)];
+
+impl Board {
+    fn field_str(&self, i: usize) -> String {
+        let b = self.board[i];
+        b.map(|b| b.colourize()).unwrap_or_else(|| format!("{}", i + 1))
+    }
+    fn draw_row(&self, i: usize) {
+        let off = i * 3;
+        println!(
+            "{} {} | {} | {}",
+            TAB,
+            self.field_str(off),
+            self.field_str(off + 1),
+            self.field_str(off + 2),
+        )
+    }
+    fn draw(&self) {
+        const ROW_DIVIDER: &str = concat!(tab!(), "--- --- ---");
+        println!();
+        self.draw_row(0);
+        println!("{}", ROW_DIVIDER);
+        self.draw_row(1);
+        println!("{}", ROW_DIVIDER);
+        self.draw_row(2);
+        println!();
+    }
+    #[inline]
+    fn check_line(&self, a: usize, b: usize, c: usize) -> bool {
+        match (self.board[a], self.board[b], self.board[c]) {
+            (Some(a), Some(b), Some(c)) => a == b && b == c,
+            _ => false,
+        }
+    }
+    #[inline]
+    fn winner(&self) -> bool {
+        WIN_CASES.iter().any(|&(a, b, c)| self.check_line(a, b, c))
+    }
 }
 
 fn main() {
-    let mut player = "X";
-    let mut board = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
-    let mut turns = 0;
-    loop {
-        if turns == 9 {
+    let mut board = Board::default();
+    let mut player = Player::X;
+
+    for turn in 1.. {
+        board.draw();
+
+        if turn > board.board.len() {
             println!("Turns out there are no available spots left.");
             println!("Game has tied.");
             break;
         }
-        draw_board(board);
-        let move_ = get_move(player);
-        if available(board, move_) {
-            turns += 1;
-            board[move_] = player;
-            if winner(board) {
+
+        let play = player.get_move();
+        let play_square = &mut board.board[play];
+        if play_square.is_none() {
+            *play_square = Some(player);
+            if board.winner() {
                 clear();
-                println!("Player {} wins on move {}!!", player, turns);
-                draw_board(board);
+                println!("Player {} wins on move {}!!", player, turn);
+                board.draw();
                 break;
             }
-            if player == "X" {
-                player = "O";
-            } else {
-                player = "X"
-            }
+            player = !player;
         } else {
             println!("Invalid move. Try again.");
         }
+
     }
     println!("Bye!");
 }
